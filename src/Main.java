8c70721b31f201758b3a38230b0da42ff6502d45
emd21 <32820346+emd21@users.noreply.github.com>
2019-03-13 03:23:54 -0400

    Changed to add nodes directly

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,11 @@ public class Main {
         System.out.println("The capacity of the queue is: " + queue.getCapacity());
         System.out.println("The size of the queue is: " + queue.getSize());
 
-        // creating nodes to add to the queue:
-        BinaryHeap.Node twelve = new BinaryHeap.Node<>("twelve", 12);
-        BinaryHeap.Node eleven = new BinaryHeap.Node<>("eleven", 11);
-        BinaryHeap.Node ten = new BinaryHeap.Node<>("ten", 10);
-        BinaryHeap.Node nine = new BinaryHeap.Node<>("nine", 9);
-
         // adding to the queue:
-        queue.insert(twelve,12);
-        queue.insert(eleven,11);
-        queue.insert(ten,10);
-        queue.insert(nine,9);
+        queue.insert("twelve",12);
+        queue.insert("eleven",11);
+        queue.insert("ten",10);
+        queue.insert("nine",9);
 
         // checking the size capacity of the queue:
         System.out.println("The capacity of the queue is: " + queue.getCapacity()); // capacity is still 3, is this normal?
